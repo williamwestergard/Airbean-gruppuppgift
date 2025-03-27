c3d29503b1f96544b4b4af404f11690242943d36
williamwestergard <williamwestergardd@gmail.com>
2025-03-27 21:39:42 +0100

    Uppdaterad projekt template med bättre struktur

---
+++ src/db.js
@@ -0,0 +1,9 @@
+// Ansluter till "./db/database.db" så att databas-informationen sparas permanent.
+// Annars försvinner informationen när servern startars om.
+const sqlite3 = require("sqlite3").verbose();
+const db = new sqlite3.Database("./db/database.db", (err) => {
+  if (err) console.error(err.message);
+  else console.log("Connected to database.db");
+});
+
+module.exports = db;
--- src/server.js
+++ src/server.js
@@ -1,7 +1,15 @@
-const express = require("express");
+// Skriv "npm start" för att starta upp servern.
+// [nodemon] gör man inte behöver starta om servern för att se ändringarna.
 
+const express = require("express");
 const app = express();
 const PORT = 4001;
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send(`Den här texten är på startsidan!`);
+});
 
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
