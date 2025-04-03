38f90711680038eac66b27c97a5bba069b18822e
williamwestergard <williamwestergardd@gmail.com>
2025-04-03 14:28:14 +0200

    William och Hassan kombinerad kod

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,7 +1,10 @@
 // Test kod för att servern inte ska krascha.
 // // Ska kopplas till models-filerna
-module.exports = {
-  get: (req, res) => {
-    res.send("Användare: Bertil Bertilsson");
-  },
+const UserModel = require("../models/userModel(old)");
+
+const findUser = async (req, res) => {
+  const { userId } = req.body;
+  if (!userId) return res.status(400).json({ message: "Användare hittas ej" });
 };
+
+module.exports = {};
